1c7ba2b49ebc263926b2e583590b7293cde03a66
rayallen20c@imooc.com <452857163@qq.com>
2023-03-19 23:58:18 +0800

    07-集成JWT 完成

---
+++ 07-集成JWT/mall/userapi/internal/logic/userapilogic.go
@@ -0,0 +1,108 @@
+package logic
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"rpc-common/user/types/user"
+	"time"
+
+	"userapi/internal/svc"
+	"userapi/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserapiLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserapiLogic {
+	return &UserapiLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserapiLogic) Userapi(req *types.Request) (resp *types.Response, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
+
+func (l *UserapiLogic) Register(req *types.Request) (resp *types.Response, err error) {
+	// todo: add your logic here and delete this line
+	// 一般做超时上下文
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	saveReq := &user.UserRequest{
+		Name:   req.Name,
+		Gender: req.Gender,
+	}
+	saveResp, err := l.svcCtx.UserRpc.SaveUser(ctx, saveReq)
+	if err != nil {
+		return nil, err
+	}
+	resp = &types.Response{
+		Message: "success",
+		Data:    saveResp,
+	}
+	return resp, nil
+}
+
+func (l *UserapiLogic) GetUser(t *types.GetUserRequest) (resp *types.GetUserResponse, err error) {
+	// 认证通过后 从token中获取userId
+	userId := l.ctx.Value("userId")
+	fmt.Printf("获取到的token内容为: %s \n", userId)
+	userIdStr := string(userId.(json.Number))
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	userResponse, err := l.svcCtx.UserRpc.GetUser(ctx, &user.IdRequest{Id: userIdStr})
+	if err != nil {
+		return nil, err
+	}
+	resp = &types.GetUserResponse{
+		Message: "success",
+		Data:    userResponse,
+	}
+	return resp, nil
+}
+
+func (l *UserapiLogic) Login(t *types.LoginRequest) (resp *types.LoginResponse, err error) {
+	userId := 1
+	secret := l.svcCtx.Config.Auth.Secret
+	expireIn := l.svcCtx.Config.Auth.ExpireIn
+	signAt := time.Now().Unix()
+	token, err := genToken(secret, signAt, expireIn, int64(userId))
+	if err != nil {
+		return nil, err
+	}
+	resp = &types.LoginResponse{
+		Message: "succcess",
+		Data: map[string]string{
+			"token": token,
+		},
+	}
+	return resp, nil
+}
+
+// genToken 生成token
+// secret:密钥
+// signAt: 签发时间
+// expireIn: token生命周期
+// userId: 用户ID
+func genToken(secret string, signAt, expireIn, userId int64) (string, error) {
+	claims := make(jwt.MapClaims)
+	// Tips: 以下2个key的值是jwt.MapClaims预制的 必须这么写key值
+	claims["exp"] = signAt + expireIn
+	claims["iat"] = signAt
+	// Tips: 这个key的值可以自定义的 后续验证token通过后 该K-V将会被存入上下文中
+	claims["userId"] = userId
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims = claims
+	return token.SignedString([]byte(secret))
+}
